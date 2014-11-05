2f85cd4615b68a8b98abcb14c96f527ed5a110f9
ziyang <ziyang0621@gmail.com>
2014-11-05 15:22:46 -0800

    project updates

---
+++ app/src/main/java/com/example/ziyang0621/funfacts/ColorWheel.java
@@ -0,0 +1,41 @@
+package com.example.ziyang0621.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by ziyang0621 on 11/5/14.
+ */
+public class ColorWheel {
+
+    public String mColors[] = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    public int getColor() {
+
+        String color = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random(); // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+
+        color = mColors[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+    }
+}
--- app/src/main/java/com/example/ziyang0621/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/ziyang0621/funfacts/FunFactsActivity.java
@@ -1,17 +1,22 @@
 package com.example.ziyang0621.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,55 +25,23 @@ public class FunFactsActivity extends Activity {
 
         // Declare our view variables and assign them the Views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built."};
-
-                // The button was clicked, so update the face label with a new fact
-                String fact = "";
-
-                // Randomly select a fact
-                Random randomGenerator = new Random(); // Construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(facts.length);
-
-                fact = facts[randomNumber];
-
+                String fact = mFactBook.getFact();
                 // Update the label with our dynamic fact
                 factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
